29a09b79d42475de6832ef3a2ad02e05ba26eef7
ZPX <894517069@qq.com>
2019-01-09 18:59:28 +0800

    修改退出功能

--- itl-manager/itl-manager-controller/src/main/java/com/zpx/itl/controller/UserLoginController.java
+++ itl-manager/itl-manager-controller/src/main/java/com/zpx/itl/controller/UserLoginController.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zpx.itl.pojo.User;
